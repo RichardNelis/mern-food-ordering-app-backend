d6f028ed269b170c163b1dd1092e325cd31ab3ab
Richard Nelis de Souza <souza.richard33@hotmail.com>
2024-03-17 13:59:43 -0300

    Adicionado cloud de imagem e criação do restaurante

---
+++ src/controllers/MyRestaurantController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import Restaurant from "../models/restaurant";
+import claudinary from "cloudinary";
+import mongoose from "mongoose";
+
+const createMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
+
+    if (existingRestaurant) {
+      return res
+        .status(409)
+        .json({ message: "User restaurant already exists" });
+    }
+
+    const image = req.file as Express.Multer.File;
+    const base64Image = Buffer.from(image.buffer).toString("base64");
+    const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+
+    const uploadResponse = await claudinary.v2.uploader.upload(dataURI);
+
+    const restaurant = new Restaurant(req.body);
+    restaurant.imageUrl = uploadResponse.url;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    restaurant.lastUpdated = new Date();
+
+    await restaurant.save();
+
+    res.status(201).send(restaurant);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const getMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found!" });
+    }
+
+    return res.json(restaurant);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Erro fetching restaurant" });
+  }
+};
+
+export default { createMyRestaurant, getMyRestaurant };
